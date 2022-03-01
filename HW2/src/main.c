99076fc88f4664d8dca914c6cf2b542a7603e356
Bjorn Isaacson <76632398+baisaacson@users.noreply.github.com>
2022-02-28 22:06:20 -0500

    Update main.c

--- HW2/src/main.c
+++ HW2/src/main.c
@@ -26,36 +26,36 @@
 #include "esp_system.h"
 #include <time.h>
 
-#define BILLION  1000000000.0
+#define BILLION  1000000000.0 // 1 billion
 
 
 //ADC Channels
-#define ADC2_EXAMPLE_CHAN0          ADC1_CHANNEL_0
+#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_2
 
-static const adc_bits_width_t width = ADC_WIDTH_BIT_13;
+//ADC Sampling
+static const adc_bits_width_t width = ADC_WIDTH_BIT_DEFAULT;
 
-
-static const char *TAG_CH[2][10] = {{"ADC1_CH2"}, {"ADC2_CH0"}};
-
-static int adc_raw[2][10];
+static const char *TAG_CH[2][10] = {{"ADC1_CH2"}, {"ADC2_CH0"}}; //ADC Channels
+static int adc_raw[2][10]; //ADC raw data
 
 void app_main(void)
 {
-    struct timespec start, end;
-
+    struct timespec start, end; //Time variables
+    adc1_config_width(width); //Configure ADC1 to read 12 bits
     while (1) {
-        clock_gettime(CLOCK_REALTIME, &start);
-        adc_raw[0][0] = adc2_get_raw(ADC2_EXAMPLE_CHAN0, width,&adc_raw[1][0]);
-        clock_gettime(CLOCK_REALTIME, &end);
-        double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;
+        clock_gettime(CLOCK_REALTIME, &start); //Start time
+        adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0); //Read ADC1
+        clock_gettime(CLOCK_REALTIME, &end); //End time
+        double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION; //Calculate time spent
  
-        ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[1][0]);
-        ESP_LOGI(TAG_CH[0][0], "The elapsed time is %f seconds", time_spent);
+        ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[0][0]); //Print raw data
+        ESP_LOGI(TAG_CH[0][0], "The elapsed time is %f seconds", time_spent); //Print time spent
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(1000)); //Delay 1 second
     }
 }
 
 //Have removed calibration of the data, checks, initializations, etc.
 //Added width for resolution. Only avalible for ADC2
 //added clock_gettime() for time stamping. Tried clock() but it was not accurate enough.
+//Changing the bit width other than default using adc1_config_width() is not supported.
