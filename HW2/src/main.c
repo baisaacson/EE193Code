4def06ae2340f2f4ab870b29ee05cc87df5eb956
Bjorn Isaacson <76632398+baisaacson@users.noreply.github.com>
2022-03-26 21:00:13 -0400

    Update main.c

--- HW2/src/main.c
+++ HW2/src/main.c
@@ -30,7 +30,7 @@
 
 
 //ADC Channels
-#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_2
+#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_2 // GPIO34
 
 //ADC Sampling
 static const adc_bits_width_t width = ADC_WIDTH_BIT_DEFAULT;
@@ -38,10 +38,20 @@ static const adc_bits_width_t width = ADC_WIDTH_BIT_DEFAULT;
 static const char *TAG_CH[2][10] = {{"ADC1_CH2"}, {"ADC2_CH0"}}; //ADC Channels
 static int adc_raw[2][10]; //ADC raw data
 
+//ADC calibration
+#define DEFAULT_VREF 1100
+#define NO_OF_SAMPLES 64 //Taking 64 samples per reading
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc_unit_t unit = ADC_UNIT_1;
+
 void app_main(void)
 {
-    struct timespec start, end; //Time variables
+    //struct timespec start, end; //Time variables
     adc1_config_width(width); //Configure ADC1 to read 12 bits
+
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t)); //Allocate memory for ADC calibration
+    esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars); //Calibrate ADC
     while (1) {
        // clock_gettime(CLOCK_REALTIME, &start); //Start time
         adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0); //Read ADC1
@@ -49,9 +59,10 @@ void app_main(void)
         //double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION; //Calculate time spent
  
         //Convert adc_reading to voltage in mV
-        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_raw[0][0]);
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_raw[0][0], adc_chars);
 
-        ESP_LOGI(TAG_CH[0][0], "raw  data: %d ", adc_raw[0][0]); //Print raw data
+        //ESP_LOGI(TAG_CH[0][0], "raw data: %d ", adc_raw[0][0]); //Print raw data
+        ESP_LOGI(TAG_CH[0][0], "voltage: %d ", voltage); //Print raw data
         //ESP_LOGI(TAG_CH[0][0], "The elapsed time is %f seconds", time_spent); //Print time spent
 
         vTaskDelay(pdMS_TO_TICKS(1000)); //Delay 1 second
